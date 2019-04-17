18979eed5ad816282c4770aaa92933f45fbb9fc8
Daniel Hu <dan3388a@gmail.com>
2019-04-17 21:23:17 +1000

    Upload FB Bot

---
+++ messages.php
@@ -0,0 +1,44 @@
+<?php 
+
+//set Message
+if($messageText == "hi") {
+    $answer = "Hello";
+}
+
+if($messageText == "weather") {
+	$text = "The current temperature is ".round($temperature,1)."°C";
+	$answer = $text;
+}
+
+$response = [
+    'recipient' => [ 'id' => $senderId ],
+    'message' => [ 'text' => $answer ]
+];
+
+if($messageText != "hi" && $messageText != "weather" && $messageText != "more") {
+	$answer = 'Perhaps you may try "hi", "weather" or "more"!';
+}
+
+if($messageText == "more") {  
+ 	$answer = ["attachment"=>[
+      "type"=>"template",
+      "payload"=>[
+        "template_type"=>"button",
+        "text"=>"What do you want to do next?",
+        "buttons"=>[
+          [
+            "type"=>"web_url",
+            "url"=>"https://weather.hyhu.me",
+            "title"=>"Show Website"
+          ]
+        ]
+      ]
+      ]];
+
+    $response = [
+    	'recipient' => [ 'id' => $senderId ],
+    	'message' => $answer 
+	];
+}
+
+?>
