0a1fa813747babed6a59e7b6ec9102d898627e2b
Daniel Hu <dan3388a@gmail.com>
2019-04-21 21:12:01 +1000

    Bot bugfix, update Bot Message
    Bot Message now can retrieve (GET) and reply user's name

--- messages.php
+++ messages.php
@@ -1,44 +1,43 @@
 <?php 
 
+// GET retrieve sender's name
+$url = 'https://graph.facebook.com/'.$senderId.'?fields=name&access_token='.$accessToken;
+$jsondata = json_decode(file_get_contents($url));
+$senderName = $jsondata->name;
+
 //set Message
 if($messageText == "hi") {
-    $answer = "Hello";
+  $answer = ['text' => "Hello, ".$senderName];
+  
+} elseif($messageText == "weather") {
+	$answer = ['text' => "The current temperature is ".round($temperature,1)."°C"];
+
+} elseif($messageText == "more") {  
+ 	$answer = ["attachment"=>[
+    "type"=>"template",
+    "payload"=>[
+      "template_type"=>"button",
+      "text"=>"What do you want to do next?",
+      "buttons"=>[
+        [
+          "type"=>"web_url",
+          "url"=>"https://weather.hyhu.me",
+          "title"=>"Show Website"
+        ]
+      ]
+    ]
+  ]];
+
+} else {
+  $answer = ['text' => 'Perhaps you may try "hi", "weather" or "more"!'];
+
 }
 
-if($messageText == "weather") {
-	$text = "The current temperature is ".round($temperature,1)."°C";
-	$answer = $text;
-}
 
-if($messageText != "hi" && $messageText != "weather" && $messageText != "more") {
-	$answer = 'Perhaps you may try "hi", "weather" or "more"!';
-}
 
 $response = [
     'recipient' => [ 'id' => $senderId ],
-    'message' => [ 'text' => $answer ]
+    'message' => $answer
 ];
-
-if($messageText == "more") {  
- 	$answer = ["attachment"=>[
-      "type"=>"template",
-      "payload"=>[
-        "template_type"=>"button",
-        "text"=>"What do you want to do next?",
-        "buttons"=>[
-          [
-            "type"=>"web_url",
-            "url"=>"https://weather.hyhu.me",
-            "title"=>"Show Website"
-          ]
-        ]
-      ]
-      ]];
-
-    $response = [
-    	'recipient' => [ 'id' => $senderId ],
-    	'message' => $answer 
-	];
-}
 
 ?>
