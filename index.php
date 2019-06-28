6dbeb8693bc928dc7f92f6b6b941233978ae868a
Daniel Hu <dan3388a@gmail.com>
2019-06-29 01:43:48 +1000

    Update Grid css, ChartJS

--- index.php
+++ index.php
@@ -5,18 +5,84 @@ include "weather_data.php";
 <html>
 	<head>
 		<link rel="stylesheet" href="css/styles.css">
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.4.0/Chart.min.js"></script>
 	</head>
 	<body>
-		<div id="middle">
-			<h1>Forecast</h1>
-			<p>Current Temperature: <?php echo round($temperature,1)?>&#8451</p>
-			<p>Chance of Rain Today: <?php echo ($precipProbability*100)?>%</p>
-			<p>Highest Temperature will be <?php echo round($todayTempHigh,1)?>&#8451 at <?php echo $highestTime?>:00</p>
-			<p>The Temperature at 8:00 will be <?php echo round($eightAmTemp,1)?>&#8451</p>
-			<p>The Temperature at 20:00 will be <?php echo round($eightPmTemp,1)?>&#8451</p>
-			<p>Today's highest UV Index: <?php echo $uvindex ?>(<?php echo $uvscale ?>)</p>
-			<p>Most recent PM2.5: <?php echo $pm25 ?>(<?php echo $pmscale ?>)</p>
-			<p><?php echo $dailySummary?></p>
+		<div class="grid-container">
+  			<div class="grid-item">
+  				<h1>Chance To Rain</h1>
+				<p>Location: <?php echo $city; ?></p>
+				<p>Current Temperature: <?php echo round($temperature,1)?>&#8451</p>
+				<p>Chance of Rain Today: <?php echo ($precipProbability*100)?>%</p>
+				<p>Highest Temperature will be <?php echo round($todayTempHigh,1)?>&#8451 at <?php echo $highestTime?>:00</p>
+				<p>The Temperature at 8:00 will be <?php echo round($eightAmTemp,1)?>&#8451</p>
+				<p>The Temperature at 20:00 will be <?php echo round($eightPmTemp,1)?>&#8451</p>
+				<p><?php echo $dailySummary?></p>
+  			</div>
+  			<div class="grid-item">
+  				<canvas id="sevenDayTemp" width="400" height="200"></canvas>
+  			</div>
+  			<div class="grid-item">
+  				<p>Today's highest UV Index: <?php echo $uvindex ?>(<?php echo $uvscale ?>)</p>
+  			</div>  
+  			<div class="grid-item">
+  				<p>Most recent PM2.5: <?php echo $pm25 ?>(<?php echo $pmscale ?>)</p>
+  			</div>
 		</div>
 	</body>
+
+<script>
+  	var ctx = document.getElementById( "sevenDayTemp" ),
+  		example = new Chart(ctx, {
+  			type: "line", // chart type
+  			data: {
+  				labels: <?php echo json_encode($weekday) ?>,
+        		datasets: [{
+            		label: 'Highest Temperature',
+            		fill: false,
+            		backgroundColor: 'rgb(255, 99, 132)',
+            		borderColor: 'rgb(255, 99, 132)',
+            		data: <?php echo json_encode($weeklyHighTemp) ?>,
+            		yAxisID: 'y-axis-1'
+        		}, {
+            		label: 'Lowest Temperature',
+            		fill: false,
+            		backgroundColor: 'rgb(60, 163, 232)',
+            		borderColor: 'rgb(60, 163, 232)',
+            		data: <?php echo json_encode($weeklyLowTemp) ?>,
+            		yAxisID: 'y-axis-1'
+        		}, {
+            		label: 'Chance To Rain',
+            		fill: false,
+            		backgroundColor: 'rgb(255, 204, 97)',
+            		borderColor: 'rgb(255, 204, 97)',
+            		data: <?php echo json_encode($weeklyChanceToRain) ?>,
+            		yAxisID: 'y-axis-2'
+        		}]
+  			},
+  			options: {
+  				scales: {
+						yAxes: [{
+							type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+							display: true,
+							position: 'left',
+							id: 'y-axis-1',
+						}, {
+							type: 'linear', // only linear but allow scale type registration. This allows extensions to exist solely for log scale for instance
+							display: true,
+							position: 'right',
+							id: 'y-axis-2',
+							ticks: {
+								min: 0,
+								max: 1
+							},
+							// grid line settings
+							gridLines: {
+								drawOnChartArea: false, // only want the grid lines for one axis to show up
+							},
+						}],
+					}
+  			}
+  		});
+</script>
 </html>
--- weather_data.php
+++ weather_data.php
@@ -1,16 +1,24 @@
 <?php 
 include "parameters.php";
-$geo_lati = "-37.7963689";
-$geo_long = "144.9611738";
+//$geo_lati = "-37.7963689";
+//$geo_long = "144.9611738";
 $timezone = 10;
 $unit = "units=si";
+
+$userIp = getUserIpAddr();
+$geojson = file_get_contents("http://ip-api.com/json/".$userIp);
+$geodata = json_decode($geojson);
+$geo_lati = $geodata->lat;
+$geo_long = $geodata->lon;
+$city = $geodata->city;
+
 $jsonfile = file_get_contents("https://api.darksky.net/forecast/".$darksky_token."/".$geo_lati.",".$geo_long."?".$unit."&exclude=minutely&exclude=alert$exclude=flags");
 $jsonAir = file_get_contents("https://api.waqi.info/feed/geo:".$geo_lati.";".$geo_long."/?token=".$air_qua_token);
 
 $jsondata = json_decode($jsonfile);
 $airdata = json_decode($jsonAir);
 $temperature = $jsondata->currently->temperature;
-$precipProbability = $jsondata->currently->precipProbability;
+$precipProbability = $jsondata->daily->data[0]->precipProbability;
 $todayTempHigh = $jsondata->daily->data[0]->temperatureHigh;
 $todayTempHighTime = $jsondata->daily->data[0]->temperatureHighTime;
 // Convert it into 24hrs + 10hrs (To Melbourne Time)
@@ -21,7 +29,23 @@ $uvindex = $jsondata->daily->data[0]->uvIndex;
 $pm25 = $airdata->data->iaqi->pm25->v;
 $pm25time = $airdata->data->time->s;
 
-include "scale.php";
+
+$weeklyHighTemp = [];
+$weeklyLowTemp = [];
+$weeklyChanceToRain = [];
+$weekday = [];
+for ($i=1; $i<8; $i++) {
+	$tempHigh = $jsondata->daily->data[$i]->temperatureHigh;
+	$tempLow = $jsondata->daily->data[$i]->temperatureLow;
+	$chanceToRain = $jsondata->daily->data[$i]->precipProbability;
+	$day = $jsondata->daily->data[$i]->time;
+	array_push($weeklyHighTemp, $tempHigh);
+	array_push($weeklyLowTemp, $tempLow);
+	array_push($weeklyChanceToRain, $chanceToRain);
+	array_push($weekday, date('D', $day));
+}
+
+include "scale.php"; // The descriptions of the UV / PM2.5 score.
 
 // Retrieve the temperature for 8am and 8pm
 $eightAmTemp = 0;
@@ -34,5 +58,18 @@ for ($x=0; $x<24; $x++) {
 	} elseif (($time+$timezone)%24 == 20) {
 		$eightPmTemp = $jsondata->hourly->data[$x]->temperature;
 	}
+}
+
+function getUserIpAddr(){
+    if(!empty($_SERVER['HTTP_CLIENT_IP'])){
+        //ip from share internet
+        $ip = $_SERVER['HTTP_CLIENT_IP'];
+    }elseif(!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+        //ip pass from proxy
+        $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+    }else{
+        $ip = $_SERVER['REMOTE_ADDR'];
+    }
+    return $ip;
 }
 ?>
